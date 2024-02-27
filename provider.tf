a128dec34d3eef24626b8e7725a7c82ce56d043c
sadi <sadikirkbes@gmail.com>
2024-02-27 20:38:22 +0300

    change commend

--- provider.tf
+++ provider.tf
@@ -26,7 +26,6 @@ backend "remote" {
 	}
 }
 
-/*
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
@@ -39,7 +38,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 
 }
-*/
 
 provider "aws" {
   region = "us-west-2"
